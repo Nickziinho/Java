edde1b59bb36ff6a3e0068d53c0bb58fa615f550
Nicolas Ramos <143462279+Nickziinho@users.noreply.github.com>
2025-04-07 20:15:32 -0300

    Create Operadores.java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = numero + 1;
+        numero ++;
+
+        System.out.println(numero);
+    }
+}
