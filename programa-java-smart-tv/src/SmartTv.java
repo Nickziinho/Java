0c98e9532cf4bbe79aecf6f67479e14e764dd43f
Nicolas Ramos <143462279+Nickziinho@users.noreply.github.com>
2025-04-08 20:45:41 -0300

    Exercicios

---
+++ programa-java-smart-tv/src/SmartTv.java
@@ -0,0 +1,25 @@
+public class SmartTv {
+    // Atributos da classe SmartTv
+    // Atributos são variáveis que pertencem a classe SmartTv
+    // Atributos são características da classe SmartTv
+    boolean ligada=false;
+    int canal=1;
+    int volume=25;
+
+    public void ligar() {
+        ligada = true;
+    }
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+    public void diminuirVolume() {
+        volume--;
+    }
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+}
---
+++ programa-java-smart-tv/src/Usuario.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        // A classe Scanner foi criada para ler o canal desejado pelo usuário
+        SmartTv smartTv = new SmartTv();
+        // Criação de um objeto SmartTv
+        
+        System.out.println("Digite o canal desejado: ");
+        int canal = scanner.nextInt();
+        smartTv.canal = canal;
+        // Aqui o usuário digita o canal desejado
+        // O canal digitado pelo usuário é atribuído ao atributo canal da classe SmartTv
+        // O atributo canal da classe SmartTv é um inteiro
+        
+        // Atributos da classe SmartTv
+        // Aqui são imprimidos os atributos da classe SmartTv
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+        
+        System.out.println("_______________________________");
+        if (smartTv.ligada == false) {
+            System.out.println("TV Ligada? " + smartTv.ligada);
+        }
+        System.out.println("_______________________________");
+        // Ligando a TV
+        smartTv.ligar();
+        // Atributos da classe SmartTv
+        // Aqui são imprimidos os atributos da classe SmartTv
+        smartTv.diminuirVolume();
+        // Diminuindo o volume da TV em 1 unidade
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println("_______________________________");
+        // Aumentando o volume
+    }
+}
