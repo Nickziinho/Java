336045d3a9242ea8d76cb2cc43bf85895cc2601f
Nicolas Ramos <143462279+Nickziinho@users.noreply.github.com>
2025-04-14 20:44:54 -0300

    Atualização

--- ContaBanco/src/ContaBanco.java
+++ ContaBanco/src/ContaBanco.java
@@ -8,10 +8,13 @@ public class ContaBanco {
 
         System.out.println("Por favor, digite o seu nome:");
         String nomeCliente = scanner.nextLine();
+        ContaTerminal.nomeCliente = nomeCliente;
         
         while (true) {
             System.out.println("Por favor, digite o número da agência:");
             String agencia = scanner.nextLine();
+            // Verifica se a agência tem 4 dígitos e se são números
+            // Se sim, atribui à variável agencia da classe ContaTerminal
             if (agencia.length() == 4 && agencia.matches("\\d{4}")) {
                 ContaTerminal.agencia = agencia;
                 break;
@@ -31,6 +34,21 @@ public class ContaBanco {
 
         scanner.close();
 
-        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é "+ ContaTerminal.agencia + ", conta " + ContaTerminal.numero + " e seu saldo " + ContaTerminal.saldo + " já está disponível para saque.");
+        System.out.println("Olá " + ContaTerminal.nomeCliente.toUpperCase() + " obrigado por criar uma conta em nosso banco, sua agência é " + String.format("%s-%s", ContaTerminal.agencia.substring(0, 3), ContaTerminal.agencia.charAt(3)) + ", conta " + ContaTerminal.numero + " e seu saldo R$" + String.format("%.2f" ,ContaTerminal.saldo) + " já está disponível para saque.");
+        /*O que tá acontecendo aqui:
+        %s → é um placeholder pra string
+        
+        String.format() vai substituir cada %s pelos argumentos que vêm depois
+        
+        Ou seja:
+        
+        Primeiro %s → substring(0, 3) = os três primeiros dígitos
+        
+        Segundo %s → charAt(3) = o último dígito
+        
+        UpperCase() → transforma a string em maiúscula
+        
+        String.format("%.2f" ,ContaTerminal.saldo) → formata o saldo com duas casas decimais
+        */
     }
 }
